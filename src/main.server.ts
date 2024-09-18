efb7b0914f0bb21109bc22d3c3e67aabe3156e5e
Robert Castro <robertsmith86@hotmail.com>
2024-09-18 14:35:21 -0300

    modificado pastas para o curso angular

---
+++ src/main.server.ts
@@ -0,0 +1,19 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
+
+const bootstrap = () => bootstrapApplication(AppComponent, config);
+
+export default bootstrap;
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'PrimeiroApp';
+}
